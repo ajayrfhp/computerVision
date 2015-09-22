d9bfbd9319fa778dc6ba55fdc09eb4d0c75e4625
ajayrfhp <ajayrfhp1710@gmail.com>
2015-09-23 00:48:05 +0530

    added pickle

--- chapter1/edgefilter.py
+++ chapter1/edgefilter.py
@@ -4,24 +4,39 @@ import numpy
 from numpy import *
 from skimage import color
 from convolution import *
-from scipy import ndimage
+import pickle
+import sys
+
+method = sys.argv[1:]
+print method[0]
+
+if(method[0] == 'edgeDetect'):
+	print "Detecting edges"
+	img = color.rgb2gray(mpimg.imread('../pictures/cycle.jpg'))
+	maskx = numpy.array([[-1,0,1],[-2,0,2],[-1,0,1]])
+	masky = numpy.transpose(maskx)
+	img2 = convolve(img,maskx) 
+	img3 = convolve(img,masky)
+	finalImg = numpy.hypot(img2,img3)
+	
+	f = open('data.pkl','wb')
+	pickle.dump(finalImg,f)
+	f.close()
+	print "dumping data"
+
+	plt.imshow(finalImg,cmap = plt.get_cmap('gray'))
+	plt.show()
 
 
 
-img = color.rgb2gray(mpimg.imread('../pictures/cycle.jpg'))
-
-maskx = numpy.array([[-1,0,1],[-2,0,2],[-1,0,1]])
-masky = numpy.transpose(maskx)
-img2 = convolve(img,maskx) 
-img3 = convolve(img,masky)
-
-finalImg = numpy.hypot(img2,img3)
-
-
-plt.imshow(finalImg,cmap = plt.get_cmap('gray'))
-plt.show()
-
-
+elif(method[0] == 'load'):
+	print "loading data"
+	f = open('data.pkl','rb')
+	finalImg = pickle.load(f)
+	f.close()
+	print "drawing image"
+	plt.imshow(finalImg,cmap = plt.get_cmap('gray'))
+	plt.show()
 
 
 
