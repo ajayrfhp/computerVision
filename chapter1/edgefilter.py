f9ea4d1c4d234393afdd9f1fe5fb029491671c04
ajayrfhp <ajayrfhp1710@gmail.com>
2015-09-22 13:39:44 +0530

    edge filter both directions

--- chapter1/edgefilter.py
+++ chapter1/edgefilter.py
@@ -8,17 +8,19 @@ from scipy import ndimage
 
 
 
-img = color.rgb2gray(mpimg.imread('../pictures/cycle.jpg'))
+img = color.rgb2gray(mpimg.imread('../pictures/ajay.jpg'))
 
-mask = numpy.array([[-1,0,1],[-2,0,2],[-1,0,1]])
+maskx = numpy.array([[-1,0,1],[-2,0,2],[-1,0,1]])
+masky = numpy.transpose(maskx)
+img2 = convolve(img,maskx) 
+img3 = convolve(img,masky)
 
-img2 = convolve(img,mask) 
+finalImg = numpy.hypot(img2,img3)
 
 
-
-plt.imshow(img2,cmap = plt.get_cmap('gray'))
+plt.imshow(finalImg,cmap = plt.get_cmap('gray'))
 plt.show()
-savefig('depayout.png')
+
 
 
 
