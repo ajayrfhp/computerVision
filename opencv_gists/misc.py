8f2057d2c2b28abe374b4887257b1db8c90e784c
ajayrfhp <ajayrfhp1710@gmail.com>
2016-04-08 15:06:47 +0530

    progress in harris detector

--- opencv_gists/misc.py
+++ opencv_gists/misc.py
@@ -1,9 +1,31 @@
 from imports import *
 
-img = cv2.imread('../pictures/ajay.jpg')
+def is_in_bounds(x,l,u):
+	return x >= l and x < u
+
+'''
+	Note, we need to obtain one score for each and every pixel. We can obtain that by operating on image level itself, there is no need to compute score pixel wise. No loops.
+	M = [ Wxx Wxy  
+	      Wxy Wyy ]
+	Score = det(M) / trace (M)      
+
+'''
+
+img = cv2.imread('../pictures/chess_board.png')
 img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+epsilon = 10**(-4)
 img_x_derivative = cv2.Sobel(img,cv2.CV_64F,1,0,ksize=5)
-img_y_derivative = cv2.Sobel(img,cv2.CV_64F,1,0,ksize=5)
+img_y_derivative = cv2.Sobel(img,cv2.CV_64F,0,1,ksize=5)
 img_w_xx = scipy.ndimage.filters.gaussian_filter(img_x_derivative*img_x_derivative,1)
-plt.imshow(img_w_xx,cmap = 'gray')
+img_w_yy= scipy.ndimage.filters.gaussian_filter(img_y_derivative*img_y_derivative,1)
+img_w_xy = scipy.ndimage.filters.gaussian_filter(img_x_derivative*img_y_derivative,1)
+det_m = img_w_xx * img_w_yy - (img_w_xy)**2
+trace_m = img_w_xx + img_w_yy
+scores = det_m / (trace_m + epsilon)
+scores[scores>=scores.mean()] = 1
+scores[scores<scores.mean()] = 0
+plt.imshow(scores,cmap = 'gray')
 plt.show()
+
+
+
